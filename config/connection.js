568ddda9d51da8051ee55adfaaeebdd4137d5bcc
Sasha <sashikers@gmail.com>
2018-03-07 19:03:08 -0800

    cosmetic

--- config/connection.js
+++ config/connection.js
@@ -10,13 +10,13 @@ var connection;
 // });
 
 if (process.env.JAWSDB_URL) {
-	connection = mysql.createConnection(process.env.JAWSDB_URL)
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 	connection = mysql.createConnection({
 		host: 'localhost',
 		user: 'root',
 		password: '',
-		database: "burger_db"
+		database: 'burger_db'
 	});
 }
 
